4514908126b28ecca67527bfee558cc2a84e5c86
Lily009204 <mosesmercy963@gmail.com>
2026-03-12 11:24:25 +0100

    Update Barak.py

--- Scripts/Barak.py
+++ Scripts/Barak.py
@@ -1,2 +1,2 @@
-Name,email,username,area of interest,programming language
-Barak johnson,barakjohnson360@gmail.com,@Barak360,genomics,python
+print("Name,email,username,area of interest,programming language")
+print("Barak johnson,barakjohnson360@gmail.com,@Barak360,genomics,python")
