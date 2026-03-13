fdac120aefcb8eba3b6db048254ab2872d0d9acc
Barak360 <barakjohnson360@gmail.com>
2026-03-13 02:11:37 +0100

    Add Perl script for Mercy Moses

--- Scripts/Mercy_moses.pl
+++ Scripts/Mercy_moses.pl
@@ -1,4 +1,4 @@
-print "Mercy Moses/n";
-print "mosesmercy963/@gmail.com/n";
-print "/@Lily009204/n";
-print Phylogenetics/n";
+print "Mercy Moses\n";
+print "mosesmercy963\@gmail.com\n";
+print "\@lily009204\n";
+print "Phylogenetics\n";
